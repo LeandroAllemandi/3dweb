58552ec057c8d16efd793d990604735c54fbb414
Leandro <leandro.allemandi@smfconsulting.es>
2025-03-01 21:55:49 -0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: "file-loader",
+      },
+    });
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
